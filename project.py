7803568c9a75df2aea5640194f72e5bd3687ee7b
selimerhan <seliwerhau@gmail.com>
2023-12-05 23:56:15 -0500

    initialized, filled and encoded the grid

--- project.py
+++ project.py
@@ -1,56 +1,88 @@
-import numpy as np
+
+    # Generate Random Colorings:
+    #     Define a set of colors: {Red, Blue, Yellow, Green}.
+    #     Implement a function to randomly select a color from the set.
+
+    # Generate a 20x20 Grid:
+    #     Initialize a 20x20 grid with all cells initially uncolored.
+    #     Implement functions to randomly select rows and columns based on the provided logic.
+    #     Apply the color to the selected rows and columns according to the instructions.
+
+    # Encode the Grid:
+    #     Use one-hot encoding to represent the colors. Assign a unique one-hot vector to each color.
+
+    # Generate Labels:
+    #     Based on your logic, determine whether the generated grid is safe or dangerous.
+    #     Assign labels accordingly.
+
+    # Repeat for Training Data:
+    #     Repeat steps 2-4 to generate a sufficient amount of training data.
+    #     Ensure a diverse set of grids with various colorings and patterns.
+ 
 import random
+import numpy as np
 
-# Function to generate a 20x20 wiring diagram with history and labels
-def generate_wiring_diagram():
-    # Initialize a 20x20 grid with placeholder values
-    wiring_diagram = np.full((20, 20), '', dtype='<U6')
 
-    # List of colors to choose from
-    colors = ["R", "B", "Y", "G"]
+colorsForRow = ["R", "B", "Y", "G"]
+colorsForCol = ["R", "B", "Y", "G"]
+sizeForRow = list(range(0,20))
+sizeForCol = list(range(0,20))
 
-    # Initialize previous values and labels
-    prev_row = np.full((20, 1), '', dtype='<U6')
-    prev_col = np.full((1, 20), '', dtype='<U6')
-    label = ''
+# Create a dictionary to map colors to one-hot vectors
+color_to_one_hot = {
+    "R": (0, 0, 0, 0, 1),
+    "B": (0, 0, 0, 1, 0),
+    "Y": (0, 0, 1, 0, 0),
+    "G": (0, 1, 0, 0, 0),
+    "U": (1, 0, 0, 0, 0)
+}
 
-    # Iterate until all cells are filled
-    while '' in wiring_diagram:
-        # Step 1: Starting with rows
-        row1 = random.randint(0, 19)
-        color1 = random.choice(colors)
-        wiring_diagram[row1, :] = color1
-        label = 'Dangerous' if 'R' in prev_row[row1] and 'Y' in prev_row[row1] and prev_row[row1].tolist().index('R') < prev_row[row1].tolist().index('Y') else 'Safe'
-        print(wiring_diagram)
-        prev_row[row1][0] = color1
+def generate_grid(grid_size):
+    grid = np.full((grid_size, grid_size), "U", dtype=str)
+    return grid
 
-        # Step 2: Switching to columns
-        col2 = random.randint(0, 19)
-        remaining_colors = np.array([c for c in colors if c != color1])
-        color2 = np.random.choice(remaining_colors)
-        wiring_diagram[:, col2] = color2
-        prev_col[0, col2] = color2
+def getRandomHotColor(random_color):
+    # getting the random hot vector color
+    random_hot_vector = color_to_one_hot[random_color]
+    return random_hot_vector
 
-        # Step 3: Switching back to rows
-        row3 = random.randint(0, 19)
-        remaining_colors = np.array([c for c in colors if c not in [color1, color2]])
-        color3 = np.random.choice(remaining_colors)
-        wiring_diagram[row3, :] = color3
-        prev_row[row3][0] = color3
+def RowColoring(grid, colorsForRow):
+    row_index = random.choice(sizeForRow)
+    random_color = random.choice(colorsForRow)     
+    colorsForRow.remove(random_color)
+    sizeForRow.remove(row_index)
+    grid[row_index, :] = random_color
+    return grid
 
-        # Step 4: Switching back to columns
-        col4 = random.randint(0, 19)
-        remaining_colors = np.array([c for c in colors if c != color3])
-        color4 = remaining_colors[0]  # Take the remaining color
-        wiring_diagram[:, col4] = color4
-        prev_col[0, col4] = color4
+def ColumnColoring(grid, colorsForCol):
+    col_index = random.choice(sizeForCol)
+    random_color = random.choice(colorsForCol)     
+    colorsForCol.remove(random_color)
+    sizeForCol.remove(col_index)
+    grid[:, col_index] = random_color
+    return grid
 
-    return wiring_diagram, label
+def encode_grid(grid, color_to_one_hot):
+    # Flatten the grid and encode each color using one-hot vectors
+    encoded_vector = []
 
-# Example usage
-generated_diagram, label = generate_wiring_diagram()
+    for row in grid:
+        for cell in row:
+            encoded_vector.extend(color_to_one_hot[cell])
 
-# Display the generated wiring diagram and label
-print("Wiring Diagram:")
-print(generated_diagram)
-print("\nLabel:", label)
+    return np.array(encoded_vector)
+
+# generating the grid
+grid = generate_grid(20)
+# filling the first row
+grid = RowColoring(grid, colorsForRow)
+# filling first col
+grid = ColumnColoring(grid,colorsForCol)
+# filling second row
+grid = RowColoring(grid, colorsForRow)
+# filling second col
+grid = ColumnColoring(grid, colorsForCol)
+
+vector = encode_grid(grid, color_to_one_hot)
+np.set_printoptions(threshold=np.inf)
+print(vector)
